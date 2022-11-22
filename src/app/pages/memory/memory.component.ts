4e9745969dfb621b7fcfaf0b26b907e55570dce8
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-11-22 02:02:18 +0100

    added memory game + styles

---
+++ src/app/pages/memory/memory.component.ts
@@ -0,0 +1,106 @@
+import { CardData } from './../../models/model';
+import { RestartDialogComponent } from './restart-dialog/restart-dialog.component';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-memory',
+  templateUrl: './memory.component.html',
+  styleUrls: ['./memory.component.scss']
+})
+export class MemoryComponent implements OnInit {
+
+  cardImages = [
+    'QnBrjY-nFUs',
+    'p5nDU-d3Y0s',
+    'wU2Nx1yoDQc',
+    'PMdNsBzn2To',
+    '9JrBiphz0e0'
+  ];
+
+  cards: CardData[] = [];
+
+  flippedCards: CardData[] = [];
+
+  matchedCount = 0;
+
+  shuffleArray(anArray: any[]): any[] {
+    return anArray.map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1]);
+  }
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  ngOnInit(): void {
+    this.setupCards();
+  }
+
+  setupCards(): void {
+    this.cards = [];
+    this.cardImages.forEach((image) => {
+      const cardData: CardData = {
+        imageId: image,
+        state: 'default'
+      };
+
+      this.cards.push({ ...cardData });
+      this.cards.push({ ...cardData });
+
+    });
+
+    this.cards = this.shuffleArray(this.cards);
+  }
+
+  cardClicked(index: number): void {
+    const cardInfo = this.cards[index];
+
+    if (cardInfo.state === 'default' && this.flippedCards.length < 2) {
+      cardInfo.state = 'flipped';
+      this.flippedCards.push(cardInfo);
+
+      if (this.flippedCards.length > 1) {
+        this.checkForCardMatch();
+      }
+
+    } else if (cardInfo.state === 'flipped') {
+      cardInfo.state = 'default';
+      this.flippedCards.pop();
+
+    }
+  }
+
+  checkForCardMatch(): void {
+    setTimeout(() => {
+      const cardOne = this.flippedCards[0];
+      const cardTwo = this.flippedCards[1];
+      const nextState = cardOne.imageId === cardTwo.imageId ? 'matched' : 'default';
+      cardOne.state = cardTwo.state = nextState;
+
+      this.flippedCards = [];
+
+      if (nextState === 'matched') {
+        this.matchedCount++;
+
+        if (this.matchedCount === this.cardImages.length) {
+          const dialogRef = this.dialog.open(RestartDialogComponent, {
+            disableClose: true
+          });
+
+          dialogRef.afterClosed().subscribe(() => {
+            this.restart();
+          });
+        }
+      }
+
+    }, 1000);
+  }
+
+  restart(): void {
+    this.matchedCount = 0;
+    this.setupCards();
+  }
+
+}
